5e00b971505141d48554af4b5bc46769849090b7
zhiqiang <zhiqwang@outlook.com>
2019-07-18 11:46:14 +0800

    Update datasets loader of object detection

--- sightseq/data/coco_dataset.py
+++ sightseq/data/coco_dataset.py
@@ -1,20 +1,64 @@
 # Copyright (c) 2019-present, Zhiqiang Wang.
 
-import torchvision
+import os
+import numpy as np
+
+from pycocotools.coco import COCO
+from fairseq.data import FairseqDataset
+
+from sightseq.data.data_utils import default_loader
 
 
-class CocoDetectionDataset(torchvision.datasets.CocoDetection):
-    def __init__(self, img_folder, ann_file, transforms):
-        super().__init__(img_folder, ann_file, transforms=transforms)
+class CocoDetectionDataset(FairseqDataset):
+    def __init__(
+        self, image_root, annotation_file,
+        transforms=None, loader=default_loader,
+    ):
+        self.image_root = image_root
+        self.coco = COCO(annotation_file)
+        self.image_ids = list(sorted(self.coco.imgs.keys()))
 
-    def __getitem__(self, idx):
-        img, target = super().__getitem__(idx)
-        image_id = self.ids[idx]
+        self.transforms = transforms
+        self.loader = loader
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: Tuple (image, target). target is the object returned by ``coco.loadAnns``.
+        """
+        coco = self.coco
+        image_id = self.image_ids[index]
+        ann_ids = coco.getAnnIds(imgIds=image_id)
+        target = coco.loadAnns(ann_ids)
+
+        path = coco.loadImgs(image_id)[0]['file_name']
+
+        image = self.loader(os.path.join(self.image_root, path))
         if self.transforms is not None:
-            img, target = self.transforms(img, target)
+            image, target = self.transforms(image, target)
 
         return {
             'id': image_id,
-            'image': img,
+            'source': image,
             'target': target,
         }
+
+    def collater(self, samples):
+        """Merge a list of samples to form a mini-batch."""
+        pass
+
+    def ordered_indices(self):
+        """
+        Return an ordered list of indices. Batches will be constructed based
+        on this order.
+        """
+        if self.shuffle:
+            return np.random.permutation(len(self))
+        else:
+            return np.arange(len(self))
